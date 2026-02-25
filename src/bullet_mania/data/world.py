80b1c9fcb2654d27fcda6255d5242101eef11188
Lucio <101882638+ItzLucio@users.noreply.github.com>
2026-02-25 16:43:53 +0100

    VFX

--- src/bullet_mania/data/world.py
+++ src/bullet_mania/data/world.py
@@ -1,14 +1,15 @@
 BULLETS: list[list] = []
 TILES: list[list] = []
 PLAYERS: list[list] = [
-    [
-        "local",
-        [0.0, 0.0], # position,
-        [0.0, 0.0], # velocity,
-        1, # side,
-        [ # animation state,
-            0, # frame,
-            0, # animation asset id
-        ]
-    ]
+    # [
+    #     "local", # player id
+    #     [0.0, 0.0], # position,
+    #     [0.0, 0.0], # velocity,
+    #     1, # side,
+    #     [ # animation state,
+    #         0, # frame,
+    #         0, # animation asset id
+    #     ],
+    #     "deagle" # gun id
+    # ]
 ]
--- src/bullet_mania/vfxManager.py
+++ src/bullet_mania/vfxManager.py
@@ -1,7 +1,10 @@
 import pygame
 import bullet_mania.data.assets as assets
 
-def draw_bullet_bloom_effect(render_surface: pygame.Surface, position, size, color):
+vignette_image = None
+muzzle_fire_image = None
+
+def draw_bullet_bloom_effect(render_surface: pygame.Surface, position: list | tuple, size: float, color: list | tuple):
     bloom_surface = pygame.Surface((size*2, size*2), pygame.SRCALPHA)
 
     pygame.draw.circle(
@@ -16,7 +19,27 @@ def draw_bullet_bloom_effect(render_surface: pygame.Surface, position, size, col
         (position[0] - size/2, position[1] - size/2)
     )
 
+def draw_muzzle_fire_effect(render_surface: pygame.Surface, position: list | tuple, alpha: int, rotation: float):
+    global muzzle_fire_image
+
+    if muzzle_fire_image is None:
+        muzzle_fire_image = assets.ASSETS["muzzle_fire"]
+    
+    if muzzle_fire_image.get_alpha() != alpha:
+        muzzle_fire_image.set_alpha(alpha)
+    
+    if rotation:
+        muzzle_fire_image = pygame.transform.rotate(muzzle_fire_image, rotation)
+
+    render_surface.blit(muzzle_fire_image, position)
+
 def draw_vignette_effect(render_surface: pygame.Surface, alpha: int):
-    vignette = assets.ASSETS["vignette"]
-    vignette.set_alpha(alpha)
-    render_surface.blit(vignette, (0, 0))
+    global vignette_image
+
+    if vignette_image is None:
+        vignette_image = assets.ASSETS["vignette"]
+    
+    if vignette_image.get_alpha() != alpha:
+        vignette_image.set_alpha(alpha)
+
+    render_surface.blit(vignette_image, (0, 0))
